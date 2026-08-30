a7ef55e89f81a278bc69bd9e7174485327479b8d
Praneeth2497 <gundlapallypraneeth6@gmail.com>
2026-08-30 12:41:57 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,23 +1,11 @@
 const express=require("express");
 const app= express();
 
-app.get("/user",(req, res)=>{
-    res.send({firstName:"praneeth", lastName:"chandra"});
-})
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
+  res.send({ firstName: "praneeth", lastName: "chandra" });
+});
 
-app.post("/user",(req, res)=>{
-    console.log("data saved successfully to database")
-    res.send({firstName:"praneeth", lastName:"chandra"});
-})
-
-app.delete("/user",(req, res)=>{
-    res.send("deleted successfully");
-})
-
-// This will match all the HTTP method API calls to /test like get, post, put, delete etc
-app.use("/test",(req, res)=>{
-res.send("hello from server");
-})
 
 app.listen(3000,()=>{
     console.log("server is listening successfully");
