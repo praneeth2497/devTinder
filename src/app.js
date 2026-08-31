db4eb7016014bb20f0935632fa606200072b83fb
Praneeth2497 <gundlapallypraneeth6@gmail.com>
2026-08-31 15:24:48 +0530

    sending dynamic data to signup api using post  http method

--- src/app.js
+++ src/app.js
@@ -3,16 +3,12 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const app= express();
 
+app.use(express.json());
 
 app.post("/signup", async (req,res)=>{
 
 //creating a new instance of the User model
-const user = new User({
-    firstName:"virat",
-    lastName: "kohli",
-    emailId:"virat@gmail.com",
-    password:"virat@123",
-});
+const user = new User(req.body);
 try{
     await user.save();
 res.send("user added successfully");
