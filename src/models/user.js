cce725d4db4ec9fe4f8583622b9285e99a23c566
Praneeth2497 <gundlapallypraneeth6@gmail.com>
2026-09-04 18:12:05 +0530

    use validator library for validating emails, password etc

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         trim: true, 
         unique: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address:"+ value);
+            }
+        },
     },
     password: {
         type: String,
